ee2fd289399cfafe413316a8d9ca51bb0fa2d4c8
Jordi Marti <heedrox@gmail.com>
2023-09-27 20:28:49 +0200

    rename executor

--- scure-cli/index.js
+++ scure-cli/index.js
@@ -5,7 +5,7 @@ const { UserTextReader } = require('./lib/user-text-reader');
 
 class ScureCliApp {
   constructor(data, parser) {
-    this.intentExecutor = new ScureCliIntentExecutor(data)
+    this.executor = new ScureCliIntentExecutor(data)
     this.userTextReader = new UserTextReader()
     this.parser = parser
   }
@@ -18,14 +18,14 @@ class ScureCliApp {
   async start() {
     const conv = getConv()
     cleanData(conv)
-    const welcomeResponse = this.intentExecutor.executeIntent('_welcome', conv, { arg: null })
+    const welcomeResponse = this.executor.executeIntent('_welcome', conv, { arg: null })
     await this.showResponse(welcomeResponse.sentence)
     let response = { sentence: '', isEnd: false }
     do {
       const text = await this.userTextReader.readUserText('(look/use/walk/pickup/inventory/answer) >')
       const { intentName, arg } = this.parser.parse(text)
       try {
-        response = this.intentExecutor.executeIntent(intentName, conv, { arg })
+        response = this.executor.executeIntent(intentName, conv, { arg })
         await this.showResponse(response.sentence)
       } catch (_) {
         await this.showResponse('No he podido procesar esta petición, lo siento.')
